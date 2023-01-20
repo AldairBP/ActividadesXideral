0f557e2d8e7497e180fbdca9b8d82615a2b6b65c
AldairBP <al1709013@upy.edu.mx>
2023-01-20 09:35:01 -0600

    Se agrega el Sigleton de vehículos

--- Semana1/Actividad2/src/Singleton.java
+++ Semana1/Actividad2/src/Singleton.java
@@ -2,7 +2,16 @@
 public class Singleton {
 
 	public static void main(String[] args) {
-
+		   Vehiculo ModelS = Vehiculo.getInstance("Tesla");
+		   Vehiculo ModelX = Vehiculo.getInstance("Tesla");
+	        
+	        // En estas lineas comprobamos que pertenecen al mismo objeto
+	        System.out.println("La marca del Model S es " + ModelS.getBrand());
+	        System.out.println("La marca del Model X es " + ModelX.getBrand());
+	        // Comprobamos por el metodo equals
+	        System.out.println("Comprobamos según el resultado de "
+	        		+ "aplicar el método equals() = " + 
+	        		ModelS.getBrand().equals(ModelX.getBrand()));
 	}
 
 }
---
+++ Semana1/Actividad2/src/Vehiculo.java
@@ -0,0 +1,33 @@
+public class Vehiculo {
+
+    private String marca;
+    private static Vehiculo vehiculo;
+
+    // Se genera un contructor
+    private Vehiculo(String modelo) {
+        this.marca = modelo;
+        System.out.println("La marca del vehículo es: " + this.marca);
+    }
+
+    // Se genera el método getInstance
+    public static Vehiculo getInstance(String marca) {
+        if (vehiculo == null){					//Si la variable de referencia no apunta a un objeto
+            vehiculo = new Vehiculo(marca);	//Creamos un objeto
+        }
+        else{
+            System.out.println("El objeto marca "+ marca + " no ha sido generado dado que ya existe uno");
+        }
+        //Retorna el valor 
+        return vehiculo; 
+    }
+    
+    //getters and setters
+	public String getBrand() {
+		return marca;
+	}
+
+	public void setBrand(String marca) {
+		this.marca = marca;
+	}
+    	
+	}
